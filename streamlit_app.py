0c248dd2d6467bf70c00b893ff2978a86158c56d
seankaedsein <119712500+seankaedsein@users.noreply.github.com>
2024-07-08 00:39:13 +0000

    added placeholder image

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,17 @@
 import streamlit as st
+from PIL import Image
 
 # Placeholder for image display
+image = Image.open('generic_CXR.jpg')
+image
 placeholder = st.empty()
-placeholder.text('Your image will be displayed here.')
+placeholder.text('Output displayed above.')
+
+if st.button("Generate Image"):
+    st.write_stream()
 
 # Single-line text input with customization
 single_line_text = st.text_input('Enter single-line text', placeholder='Type something here...')
+
+x = st.slider('x')
+st.write(x, 'squared is', x * x)
